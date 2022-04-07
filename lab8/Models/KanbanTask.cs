e2524ab02f0e8159af6ec4d0c65fe2713710b447
Sofya Mogutova <smogutova1234@gmail.com>
2022-04-07 11:34:43 +0700

    Добавьте файлы проекта.

---
+++ lab8/Models/KanbanTask.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Avalonia.Media.Imaging;
+
+namespace lab8.Models
+{
+    public class KanbanTask : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        internal void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public KanbanTask(string status = "todo")
+        {
+            this.Header = "Header";
+            this.Task = "Task";
+            this.ImagePath = "../../../Assets/note.png";
+            this.Image = new Bitmap(ImagePath);
+            this.Status = status;
+        }
+        public string Header { get; set; }
+        public string Status { get; set; }
+        public string Task { get; set; }
+        private Bitmap image;
+        public string ImagePath { get; set; }
+        public Bitmap Image
+        {
+            get => image;
+            set
+            {
+                image = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
